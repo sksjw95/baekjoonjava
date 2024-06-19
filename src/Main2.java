f8f8df3cb0e21a793530fd649c4957f68cd30012
sksjw95 <tjwldnd95@hanmail.net>
2024-06-19 10:15:48 +0900

    test2

---
+++ src/Main2.java
@@ -0,0 +1,10 @@
+/*import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A*B);
+    }
+}*/
