31bcb145fc924665493f39501fc2555db5144895
sksjw95 <tjwldnd95@hanmail.net>
2024-08-04 14:21:23 +0900

    test3

---
+++ src/Main3.java
@@ -0,0 +1,10 @@
+/*import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        double B = scanner.nextDouble();
+        System.out.println(A/B);
+    }
+}*/
