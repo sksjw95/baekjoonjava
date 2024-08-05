8a5bccad76f884c8b6d83a9b2671c066dbdf72b9
sksjw95 <tjwldnd95@hanmail.net>
2024-08-05 09:05:14 +0900

    test4

---
+++ src/Main4.java
@@ -0,0 +1,15 @@
+/*import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt() ;
+        int B = scanner.nextInt() ;
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+
+    }
+}*/
