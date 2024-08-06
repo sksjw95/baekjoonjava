cd18ef194516b51e8d8b711dea752f1d01bc8ac1
sksjw95 <tjwldnd95@hanmail.net>
2024-08-06 09:22:19 +0900

    test5

---
+++ src/Main5.java
@@ -0,0 +1,9 @@
+/*import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String A = scanner.nextLine();
+        System.out.println(A+"??!");
+    }
+}*/
