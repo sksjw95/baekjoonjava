8ef37d73a976f60eeffc3f0eae2f0edfda052423
sksjw95 <tjwldnd95@hanmail.net>
2024-08-13 09:29:16 +0900

    test12

---
+++ src/Main12.java
@@ -0,0 +1,19 @@
+/*import java.util.Scanner;
+
+public class Main12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if(a>b){
+            System.out.println(">");
+        }
+        else if(b>a){
+            System.out.println("<");
+        }
+        else{
+            System.out.println("==");
+        }
+
+    }
+}*/
