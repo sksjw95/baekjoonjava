c65ba11f03cbfdedaa95598a9ad1b1a0adf31809
sksjw95 <tjwldnd95@hanmail.net>
2024-08-31 15:22:07 +0900

    test15

---
+++ src/Main15.java
@@ -0,0 +1,25 @@
+/*import java.util.Scanner;
+
+public class Main15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if(a>0){
+            if(b>0){
+                System.out.println(1);
+            }
+            else{
+                System.out.println(4);
+            }
+        }
+        else{
+            if(b>0){
+                System.out.println(2);
+            }
+            else{
+                System.out.println(3);
+            }
+        }
+    }
+}*/
