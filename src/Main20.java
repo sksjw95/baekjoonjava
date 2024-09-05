fa1ecf15cab86d275ddecd0c90866a033cc609af
sksjw95 <tjwldnd95@hanmail.net>
2024-09-05 10:08:32 +0900

    test20

---
+++ src/Main20.java
@@ -0,0 +1,13 @@
+/*import java.util.Scanner;
+
+public class Main20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for(int i=1;i<=t;i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}*/
