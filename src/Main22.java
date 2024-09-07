4d2560b771c876fe6ed8482b04c1a8832a200336
sksjw95 <tjwldnd95@hanmail.net>
2024-09-07 13:48:46 +0900

    test22

---
+++ src/Main22.java
@@ -0,0 +1,24 @@
+/*import java.util.Scanner;
+
+public class Main22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalprice = scanner.nextInt();
+        int totalkind = scanner.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i<=totalkind; i++){
+            int price = scanner.nextInt();
+            int num = scanner.nextInt();
+            sum += price*num;
+        }
+
+        if(sum == totalprice){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+    }
+}*/
