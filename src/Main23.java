a1eb547a2840b18359fb827d795033e742d5e8da
sksjw95 <tjwldnd95@hanmail.net>
2024-09-08 15:05:37 +0900

    test23

---
+++ src/Main23.java
@@ -0,0 +1,14 @@
+/*import java.util.Scanner;
+
+public class Main23 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String a = "";
+
+        for (int i = 1; i<=(num/4); i++){
+            a+="long ";
+        }
+        System.out.println(a+"int");
+    }
+}*/
