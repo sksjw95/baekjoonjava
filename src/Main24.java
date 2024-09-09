50b5f670eb4f2fc0cc2d9ba19bc35ebfb3b48fa8
sksjw95 <tjwldnd95@hanmail.net>
2024-09-09 09:59:14 +0900

    test24

---
+++ src/Main24.java
@@ -0,0 +1,15 @@
+/*import java.util.Scanner;
+
+public class Main24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int sum ;
+        for(int i = 1; i<=num; i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            sum = a+b;
+            System.out.println(sum);
+        }
+    }
+}*/
