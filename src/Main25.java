ab07c1010ae8b75a6b2ec016abbee5a956283898
sksjw95 <tjwldnd95@hanmail.net>
2024-09-10 09:40:07 +0900

    test25

---
+++ src/Main25.java
@@ -0,0 +1,21 @@
+/*import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main25 {
+    public static void main(String[] args) throws IOException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < a; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            bw.write(sum + "\n");
+
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}*/
