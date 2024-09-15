0c2b545e49ca614b9d9f50fb22b6218412578c76
sksjw95 <tjwldnd95@hanmail.net>
2024-09-16 00:02:42 +0900

    test29

---
+++ src/Main29.java
@@ -0,0 +1,22 @@
+/*import java.util.Scanner;
+
+public class Main29
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int n = scanner.nextInt();
+
+       for(int i=1; i<=n; i++){
+           int j;
+           for( j=1; j<=n-i;j++){
+               System.out.print(" ");
+
+           }
+           for(int k=1;k<=i;k++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+    }
+}*/
+
+
