5ac596469c2b969816bfc68991db4cf262c4a56b
sksjw95 <tjwldnd95@hanmail.net>
2024-09-18 10:46:17 +0900

    test31

---
+++ src/Main31.java
@@ -0,0 +1,18 @@
+/*import java.util.Scanner;
+public class Main31 {
+    public static void main(String[] args) {
+
+              Scanner scanner = new Scanner(System.in);
+
+              while(scanner.hasNextInt()){
+                  int a = scanner.nextInt();
+                  if(scanner.hasNextInt()){
+                      int b = scanner.nextInt();
+                      System.out.println(a+b);
+                  }
+                  else{
+                      break;
+                  }
+              }
+            }
+        }*/
