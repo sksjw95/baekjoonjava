b255a02b40d5940e632bbcdef45ab9b847fb413c
sksjw95 <tjwldnd95@hanmail.net>
2024-09-19 10:00:45 +0900

    test32

---
+++ src/Main32.java
@@ -0,0 +1,20 @@
+/*import java.util.Scanner;
+
+public class Main32 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int total = 0;
+        int[] ar= new int[num];
+        for(int i=0;i<num;i++){
+            ar[i]=scanner.nextInt();
+        }
+        int ans = scanner.nextInt();
+        for(int i=0;i<num;i++){
+            if(ar[i]==ans){
+                total++;
+            }
+        }
+        System.out.println(total);
+    }
+}*/
