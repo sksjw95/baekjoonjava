edb9f880996b7c1997b833252fbfaeb0fa8ef75d
sksjw95 <tjwldnd95@hanmail.net>
2024-09-20 08:52:53 +0900

    test33

---
+++ src/Main33.java
@@ -0,0 +1,18 @@
+/* import java.util.Scanner;
+
+public class Main33 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int X = scanner.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = scanner.nextInt();
+        }
+        for(int i=0; i<N; i++){
+            if(arr[i]<X){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}*/
