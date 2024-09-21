f5ab3c77b2be7fb5dcd9aa2d829dd1c0143aef13
sksjw95 <tjwldnd95@hanmail.net>
2024-09-21 14:46:18 +0900

    test34

---
+++ src/Main34.java
@@ -0,0 +1,29 @@
+/*import java.util.Scanner;
+
+public class Main34 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int N=scanner.nextInt();
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i]=scanner.nextInt();
+        }
+        int max = arr[0];
+        for(int i=1;i<N;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+
+        }
+
+        int min = arr[0];
+        for(int i=1;i<N;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+
+        }
+        System.out.println(min+" "+max);
+    }
+}*/
