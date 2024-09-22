a76dd2744d4e4bbe7ce5fd697c0bb682ad315b2f
sksjw95 <tjwldnd95@hanmail.net>
2024-09-22 14:42:51 +0900

    test35

---
+++ src/Main35.java
@@ -0,0 +1,27 @@
+/*import java.util.Scanner;
+
+public class Main35 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[9];
+        for(int i=0;i<9;i++){
+            arr[i] = scanner.nextInt();
+        }
+        int max = arr[0];
+        int maxindex = 1;
+        for(int i=1;i<9;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                maxindex = i;
+            }
+        }
+        if(max==arr[0]){
+            System.out.println(max);
+            System.out.println(maxindex);
+        }
+        else {
+            System.out.println(max);
+            System.out.println(maxindex+1);
+        }
+    }
+}*/
