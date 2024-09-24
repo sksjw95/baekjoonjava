ef63ba7699df8db7361b5633b741ce15399e4b2a
sksjw95 <tjwldnd95@hanmail.net>
2024-09-24 09:50:07 +0900

    test36

---
+++ src/Main36.java
@@ -0,0 +1,21 @@
+/*import java.util.Scanner;
+
+public class Main36 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for(int i=0;i<M;i++){
+            int k = scanner.nextInt(),d = scanner.nextInt(),j = scanner.nextInt();
+            for(int f=k-1;f<=d-1;f++) {
+                arr[f] = j;
+            }
+
+        }
+        for(int r=0;r<N;r++){
+            System.out.print(arr[r]+" ");
+        }
+    }
+}*/
