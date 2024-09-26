21b361191168a32e9b3a0537dfb1eb6fac2322f5
sksjw95 <tjwldnd95@hanmail.net>
2024-09-26 10:20:34 +0900

    test38

---
+++ src/Main38.java
@@ -0,0 +1,25 @@
+ /*import java.util.Scanner;
+
+public class Main38 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arr[] = new int[30];
+        for(int i=0; i<30; i++){
+            arr[i]=i+1;
+
+        }
+        for(int j=0; j<30; j++){
+            int b=scanner.nextInt();
+            switch (b){
+                case 1: case 3: case 4: case 5: case 6: case 7:
+                case 9:case 10: case 11: case 12: case 13: case 14: case 15: case 16: case 17:
+                case 18:case 19: case 20: case 21: case 22: case 23: case 24: case 25: case 26:
+                case 27:case 28: case 29: case 30: continue; //break;
+                case 2: case 8: System.out.println(b);break;
+            }
+
+        }
+
+
+    }
+}*/
