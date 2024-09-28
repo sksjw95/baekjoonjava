48e6529ac73589b52d867091fc824a37e2f203cc
sksjw95 <tjwldnd95@hanmail.net>
2024-09-28 20:36:02 +0900

    test40

---
+++ src/Main40.java
@@ -0,0 +1,19 @@
+/*import java.util.Scanner;
+
+public class Main40 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int[] students = new int[30];
+
+        for(int i=0;i<28;i++){
+            int num = scanner.nextInt();
+            students[num-1]=1;
+
+        }
+        for(int i=0;i<30;i++){
+            if(students[i]==0){
+                System.out.println(i+1);
+            }
+        }
+    }
+}*/
