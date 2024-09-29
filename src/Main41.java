f54a20c20d78b09b772b126adecfe36067904918
sksjw95 <tjwldnd95@hanmail.net>
2024-09-29 12:47:04 +0900

    test41

---
+++ src/Main41.java
@@ -0,0 +1,20 @@
+/*import java.util.Scanner;
+
+public class Main41 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arr[] = new int[10];
+        int n =1;
+        for(int i=0;i<10;i++){
+            int a = scanner.nextInt();
+            arr[i]= a%42;
+
+        }
+        for(int j=0;j<9;j++){
+            if(arr[j]!=arr[j+1]){
+                n++;
+            }
+        }
+        System.out.println(n);
+    }
+}*/
