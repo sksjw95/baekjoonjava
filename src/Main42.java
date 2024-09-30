ef44463fe17e3fbd7b34769ab3bff0805213e633
sksjw95 <tjwldnd95@hanmail.net>
2024-09-30 09:17:13 +0900

    test42

---
+++ src/Main42.java
@@ -0,0 +1,26 @@
+/*import java.util.Scanner;
+
+public class Main42 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arr[] = new int[10];
+        int counts[] = new int[42];
+        int dif = 0;
+
+        for(int i=0; i<10;i++){
+            int num = scanner.nextInt();
+            arr[i] = num%42;
+        }
+
+        for(int i=0; i<10;i++){
+            counts[arr[i]]++;
+        }
+
+        for(int i=0;i<42;i++){
+            if(counts[i]>0){
+                dif++;
+            }
+        }
+        System.out.println(dif);
+    }
+}*/
