9f132dec3be6d95a756bc43b7e4a491e3c8e4e60
sksjw95 <tjwldnd95@hanmail.net>
2024-10-03 00:31:23 +0900

    test45

---
+++ src/Main45.java
@@ -0,0 +1,11 @@
+/*import java.util.Scanner;
+
+public class Main45 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String ch = scanner.nextLine();
+        int num = scanner.nextInt();
+
+        System.out.println(ch.charAt(num-1));
+    }
+}*/
