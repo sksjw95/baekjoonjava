6ec5bce6c41c1f8db814ad831234348bdb45cd89
sksjw95 <tjwldnd95@hanmail.net>
2024-10-04 09:21:19 +0900

    test46

---
+++ src/Main46.java
@@ -0,0 +1,9 @@
+/* import java.util.Scanner;
+
+public class Main46 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String ch = scanner.nextLine();
+        System.out.println(ch.length());
+    }
+*/
