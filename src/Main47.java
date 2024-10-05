545984b5475c64a783751902c698604082fb7439
sksjw95 <tjwldnd95@hanmail.net>
2024-10-05 19:20:33 +0900

    test47

---
+++ src/Main47.java
@@ -0,0 +1,16 @@
+/*import java.util.Scanner;
+
+public class Main47 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        scanner.nextLine();
+        for(int i=0;i<num;i++){
+            String ch = scanner.nextLine();
+            int chn = ch.length();
+            System.out.print(ch.charAt(0));
+            System.out.print(ch.charAt(chn-1));
+            System.out.println();
+        }
+    }
+}*/
