5165efaf648797f5ff74025977aa3488d8661b88
sksjw95 <tjwldnd95@hanmail.net>
2024-10-06 11:19:36 +0900

    test48

---
+++ src/Main48.java
@@ -0,0 +1,10 @@
+/*import java.util.Scanner;
+
+public class Main48 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.nextLine().charAt(0);
+        int b = (int)c;
+        System.out.println(b);
+    }
+}*/
