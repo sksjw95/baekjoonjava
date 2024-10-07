944749c02339dbf7c68085c40ee305e54d233ed2
sksjw95 <tjwldnd95@hanmail.net>
2024-10-07 10:23:58 +0900

    test49

---
+++ src/Main49.java
@@ -0,0 +1,21 @@
+/*import java.util.Scanner;
+
+public class Main49 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        scanner.nextLine();
+
+        String ch = scanner.nextLine();
+        int sum = 0;
+
+        for(int i=0;i<a;i++){
+            char c = ch.charAt(i);
+            int num = Character.getNumericValue(c);
+            sum+=num;
+        }
+
+
+        System.out.println(sum);
+    }
+}*/
