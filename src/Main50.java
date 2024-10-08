7141036d78a9105993b6f6905efd5b18b185436c
sksjw95 <tjwldnd95@hanmail.net>
2024-10-08 10:10:51 +0900

    test50

---
+++ src/Main50.java
@@ -0,0 +1,25 @@
+ /*import java.util.Scanner;
+
+public class Main50
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        int[] alp = new int[26];
+
+        for(int i=0;i<26;i++){
+            alp[i]=-1;
+        }
+
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(alp[ch-'a']==-1){
+                alp[ch-'a']=i;
+            }
+        }
+        for(int i=0;i<26;i++){
+            System.out.print(alp[i]+" ");
+        }
+
+    }
+}*/
