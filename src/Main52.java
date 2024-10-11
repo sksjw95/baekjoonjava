307954465e2c4033093efc35f7c6f5b82edc9e14
sksjw95 <tjwldnd95@hanmail.net>
2024-10-11 09:30:30 +0900

    test52

---
+++ src/Main52.java
@@ -0,0 +1,18 @@
+/*import java.util.Scanner;
+
+public class Main52 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().trim();
+        String[] str2 = str.split(" ");
+
+        if(str.isEmpty()){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(str2.length);
+        }
+
+
+    }
+}*/
