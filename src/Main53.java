9cf7f9a1f5e64f46d8f41e13744671a787041fa7
sksjw95 <tjwldnd95@hanmail.net>
2024-10-12 22:56:43 +0900

    test53

---
+++ src/Main53.java
@@ -0,0 +1,28 @@
+/*import java.util.Scanner;
+
+public class Main53 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.next();
+        String str2 = "";
+        String str3 = scanner.next();
+        String str4 = "";
+        for(int i=str1.length()-1;i>=0;i--){
+            str2+=str1.charAt(i);
+        }
+
+        for(int j=str3.length()-1;j>=0;j--){
+            str4+=str3.charAt(j);
+        }
+
+        int a = Integer.valueOf(str2);
+        int b = Integer.valueOf(str4);
+
+        if(a>b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+    }
+}*/
