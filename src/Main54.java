efdb0c228bc159ca4b4b682fc92356908be199b2
sksjw95 <tjwldnd95@hanmail.net>
2024-10-13 14:08:17 +0900

    test54

---
+++ src/Main54.java
@@ -0,0 +1,13 @@
+/*import java.util.Scanner;
+
+public class Main54 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        StringBuilder builder = new StringBuilder(str);
+        builder.reverse();
+        System.out.println(builder.toString());
+    }
+}
+*/
