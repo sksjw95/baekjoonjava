e41dc57497a3a5c189f8cb372c65bae1b2144986
sksjw95 <tjwldnd95@hanmail.net>
2024-10-14 10:01:51 +0900

    test55

---
+++ src/Main55.java
@@ -0,0 +1,64 @@
+/*import java.util.Scanner;
+
+public class Main55 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String[] str2 = str.split("");
+        int sum = 0;
+        String[] B = {"A", "B", "C"};
+        String[] C = {"D", "E", "F"};
+        String[] D = {"G", "H", "I"};
+        String[] E = {"J", "K", "L"};
+        String[] F = {"M", "N", "O"};
+        String[] G = {"P", "Q", "R","S"};
+        String[] H = {"T", "U", "V"};
+        String[] I = {"W", "X", "Y","Z"};
+        for (int i = 0; i < str2.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (str2[i].equals(B[j])) {
+                    sum += 3;
+                }
+            }
+            for (int j = 0; j < C.length; j++) {
+                if (str2[i].equals(C[j])) {
+                    sum += 4;
+                }
+            }
+            for (int j = 0; j < D.length; j++) {
+                if (str2[i].equals(D[j])) {
+                    sum += 5;
+                }
+            }
+            for (int j = 0; j < E.length; j++) {
+                if (str2[i].equals(E[j])) {
+                    sum += 6;
+                }
+            }
+            for (int j = 0; j < F.length; j++) {
+                if (str2[i].equals(F[j])) {
+                    sum += 7;
+                }
+            }
+            for (int j = 0; j < G.length; j++) {
+                if (str2[i].equals(G[j])) {
+                    sum += 8;
+                }
+            }
+            for (int j = 0; j < H.length; j++) {
+                if (str2[i].equals(H[j])) {
+                    sum += 9;
+                }
+            }
+            for (int j = 0; j < I.length; j++) {
+                if (str2[i].equals(I[j])) {
+                    sum += 10;
+                }
+            }
+
+
+
+        }
+        System.out.println(sum);
+    }
+}*/
