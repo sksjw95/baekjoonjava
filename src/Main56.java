6cefc15ab54f4e15ff56ef404deccdfc05f73812
sksjw95 <tjwldnd95@hanmail.net>
2024-10-16 09:30:07 +0900

    test56

---
+++ src/Main56.java
@@ -0,0 +1,14 @@
+/*import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main56 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            while(scanner.hasNextLine()){
+                String str = scanner.nextLine();
+                System.out.println(str);
+            }
+
+        }
+    }*/
