2698496bc033c767d6c4900e8612b28f1ba9fa15
sksjw95 <tjwldnd95@hanmail.net>
2024-10-18 09:48:51 +0900

    test58

---
+++ src/Main58.java
@@ -0,0 +1,106 @@
+/*import java.util.Scanner;
+
+public class Main58 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();;
+        int b = 0;
+        while( a!=1){
+
+            if(a<1){
+                a++;
+                b++;
+            }
+            else if(a>1){
+                a--;
+                b--;
+            }
+
+        }
+        System.out.print(b);
+
+        int c= scanner.nextInt();;
+        int d = 0;
+        while( c!=1){
+
+            if(c<1){
+                c++;
+                d++;
+            }
+            else if(c>1){
+                c--;
+                d--;
+            }
+
+        }
+        System.out.print(" "+d);
+
+        int e= scanner.nextInt();;
+        int f = 0;
+        while( e!=2){
+
+            if(e<2){
+                e++;
+                f++;
+            }
+            else if(e>2){
+                e--;
+                f--;
+            }
+
+        }
+        System.out.print(" "+f);
+
+        int g= scanner.nextInt();;
+        int h = 0;
+        while( g!=2){
+
+            if(g<2){
+                g++;
+                h++;
+            }
+            else if(g>2){
+                g--;
+                h--;
+            }
+
+        }
+        System.out.print(" "+h);
+
+        int i= scanner.nextInt();;
+        int j = 0;
+        while( i!=2){
+
+            if(i<2){
+                i++;
+                j++;
+            }
+            else if(i>2){
+                i--;
+                j--;
+            }
+
+        }
+        System.out.print(" "+j);
+
+        int k= scanner.nextInt();;
+        int l = 0;
+        while( k!=8){
+
+            if(k<8){
+                k++;
+                l++;
+            }
+            else if(k>8){
+                k--;
+                l--;
+            }
+
+        }
+        System.out.print(" "+l);
+
+
+    }
+
+    }*/
+
